786163416b623abf0e036b7a290905ba359f39cb
qhgill <qhgill2@gmail.com>
2024-05-13 16:23:03 -0700

    updated with real tests whoops

--- testing/characterTests.cpp
+++ testing/characterTests.cpp
@@ -17,18 +17,26 @@ TEST(getterTests, getHpTest){
 
 TEST(getterTests, getDamageTest){
     Character char1(1,1,1,1,"class1", nullptr, nullptr, nullptr);
+    int testDamage = char1.getBaseDamage();
+    EXPECT_EQ(testDamage, 1);
 }
 
 TEST(getterTests, getPRTest){
     Character char1(1,1,1,1,"class1", nullptr, nullptr, nullptr);
+    int testPR = char1.getBasePhysicalResist();
+    EXPECT_EQ(testPR, 1);
 }
 
 TEST(getterTests, getMRTest){
     Character char1(1,1,1,1,"class1", nullptr, nullptr, nullptr);
+    int testMR = char1.getBaseMagicResist();
+    EXPECT_EQ(testMR, 1);
 }
 
 TEST(getterTests, getClassNameTest){
     Character char1(1,1,1,1,"class1", nullptr, nullptr, nullptr);
+    string testName = char1.getClassName();
+    EXPECT_EQ(testName, "class1");
 }
 
 int main(int argc, char **argv) {
