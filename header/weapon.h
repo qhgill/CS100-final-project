0694ee4b680d133eb9a83256134503aa60cac4e4
jliu0411 <liu.james2523@gmail.com>
2024-05-13 16:17:04 -0700

    weapon header added

--- header/weapon.h
+++ header/weapon.h
@@ -8,13 +8,11 @@ class weapon: public item
     private:
         int damage;
         string damageType;
-        string weaponName;
 
     public:
         weapon(int damage_, string damageType_, string weaponName_);
         int getDamage() {return damage;}
         string getDamageType() {return damageType;}
-        string getWeaponName() {return weaponName;}
         int calcDamage(int level) {return level*damage;}
 
 };
