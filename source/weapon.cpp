52a6a697b853370517f8007a06324ed23be26862
jliu0411 <liu.james2523@gmail.com>
2024-05-13 16:18:15 -0700

    weapon source file

--- source/weapon.cpp
+++ source/weapon.cpp
@@ -1,5 +1,6 @@
 #include "header/weapon.h"
 
 weapon::weapon(int damage_, string damageType_, string weaponName_)
-: damage(damage_), damageType(damageType_), weaponName(weaponName_) {}
+: damage(damage_), damageType(damageType_), weaponName(weaponName_) 
+{}
 
