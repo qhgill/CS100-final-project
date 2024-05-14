ab134c592f024882767419f4efc8fb917b5966ff
jliu0411 <liu.james2523@gmail.com>
2024-05-14 14:55:04 -0700

    still need to add calcTurn function but no information to do that right now

--- source/enemy.cpp
+++ source/enemy.cpp
@@ -1,15 +1,16 @@
 #include "header/enemy.h"
 
-enemy::enemy(int level_, 
+Enemy::Enemy(int level_, 
               int health_, 
               int maxHealth_, 
               int damage_, 
               int magicResist_, 
               int physicalResist_,
+              bool isdead_,
               string name_,
               City cityName_)
 : level(level_), health(health_), maxHealth(maxHealth_), 
 damage(damage_), magicResist(magicResist_), physicalResist(physicalResist_), 
-name(name_), cityName(cityName_) {}
+isdead(isdead_), name(name_), cityName(cityName_) {}
 
 
