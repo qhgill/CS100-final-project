cb21b00dd381d613781f8374d5d56d5b400bd016
jliu0411 <liu.james2523@gmail.com>
2024-05-14 19:32:16 -0700

    weapon header

--- header/weapon.h
+++ header/weapon.h
@@ -1,16 +1,27 @@
 #include <iostream>
 #include <string>
 using namespace std;
-#include "header/item.h"
 
-class weapon: public item
+class Item
+{
+    private:
+        string name;
+    public:
+        Item():name(""){}
+        Item(string name_):name(name_){}
+        string getName() {return name;}
+};
+
+class Weapon: public Item
 {
     private:
         int damage;
         string damageType;
+        Item* info;
 
     public:
-        weapon(int damage_, string damageType_, string weaponName_);
+        Weapon():damage(0), damageType(0), info(nullptr) {}
+        Weapon(int damage_, string damageType_, Item* item1);
         int getDamage() {return damage;}
         string getDamageType() {return damageType;}
         int calcDamage(int level) {return level*damage;}
