784c41af1676b9096f60bc3436d7a80bd2a8c99e
Bestin Watts <bwatt004@xe-01.cs.ucr.edu>
2024-05-20 16:49:36 -0700

    added temporary bosstest file

--- testing/bossTests.cpp
+++ testing/bossTests.cpp
@@ -7,4 +7,18 @@
 
 TEST(getterTests, inheritanceTest) {
     BossPart1 tempBoss();
+
+    ASSERT_EQ(tempBoss.getLevel(), 0);
+    ASSERT_EQ(tempBoss.getHealth(), 0);
+    ASSERT_EQ(tempBoss.getMaxHealth(), 0);
+    ASSERT_EQ(tempBoss.getDamage(), 0);
+    ASSERT_EQ(tempBoss.getMagicResist(), 0);
+    ASSERT_EQ(tempBoss.getPhysicalResist(), 0);
+    ASSERT_EQ(tempBoss.getStatus(), false);
+    ASSERT_EQ(tempBoss.getName(), "");
+}
+
+int main(int argc, char **argv) {
+  testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
