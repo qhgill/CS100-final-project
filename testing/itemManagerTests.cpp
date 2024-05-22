46c24eeba8cba68199b9e26765ac8873c67a9cfc
Bestin Watts <bwatt004@xe-01.cs.ucr.edu>
2024-05-21 17:42:47 -0700

    fixed userTests

--- testing/itemManagerTests.cpp
+++ testing/itemManagerTests.cpp
@@ -2,3 +2,22 @@
 #include "../header/user.h"
 #include <../header/itemManager.h>
 
+
+
+TEST(itemManagerConstructor, getterFunctionTests) {
+    std::vector<Item*>* i = new std::vector<Item*>;
+    Weapon* w = new Weapon("",0, 0, "");
+    Armor* a = new Armor("", 0,0,0,0);
+    Trinket* t = new Trinket("",0,0);
+
+    ItemManager* im = new ItemManager(i, w, a, t);
+
+    ASSERT_EQ(im->getInventory(), i);
+    ASSERT_EQ(im->getEquippedWeapon(), w);
+    ASSERT_EQ(im->getEquippedArmor(), a);
+    ASSERT_EQ(im->getEquippedTrinket(), t);
+}
+
+TEST() {
+
+}
--- testing/userTests.cpp
+++ testing/userTests.cpp
@@ -2,113 +2,88 @@
 #include "../header/user.h"
 
 TEST(userConstructorTests, overloadedConstructorTest){
-    std::vector<Item*>* i = new std::vector<Item*>;
-    Weapon* w = new Weapon("",0, 0, "");
-    Armor* a = new Armor("", 0,0,0,0);
-    Trinket* t = new Trinket("",0,0);
+    
     Character* c = new Character();
     StatsManager* sm = new StatsManager();
+    ItemManager* im = new ItemManager();
 
-    User u(1, 2, 3, i, w, a, t, c, sm);
+    User u(1, 2, 3, c, sm, im);
 
     EXPECT_EQ(u.getLevel(), 1);
     EXPECT_EQ(u.getXp(), 2);
     EXPECT_EQ(u.getEncounterCount(), 3);
-    EXPECT_EQ(u.getInventory(), i);
     EXPECT_EQ(u.getCharacterClass(), c);
     EXPECT_EQ(u.getStatsManager(), sm);
+    EXPECT_EQ(u.getItemManager(), im);
 }
 
 TEST(userGetterTests, getUserLevel){
-    std::vector<Item*>* i = new std::vector<Item*>;
-    Weapon* w = new Weapon("",0, 0, "");
-    Armor* a = new Armor("", 0,0,0,0);
-    Trinket* t = new Trinket("",0,0);
+
     Character* c = new Character();
     StatsManager* sm = new StatsManager();
+    ItemManager* im = new ItemManager();
 
-    User u(1, 2, 3, i, w, a, t, c, sm);
+    User u(1, 2, 3, c, sm, im);
 
     EXPECT_EQ(u.getLevel(), 1);
 }
 
 TEST(userGetterTests, getUserXp){
-    std::vector<Item*>* i = new std::vector<Item*>;
-    Weapon* w = new Weapon("",0, 0, "");
-    Armor* a = new Armor("", 0,0,0,0);
-    Trinket* t = new Trinket("",0,0);
+    
     Character* c = new Character();
     StatsManager* sm = new StatsManager();
+    ItemManager* im = new ItemManager();
 
-    User u(1, 2, 3, i, w, a, t, c, sm);
+    User u(1, 2, 3, c, sm, im);
     EXPECT_EQ(u.getXp(), 2);
 }
 
 TEST(userGetterTests, getUserEncounterCount){
-    std::vector<Item*>* i = new std::vector<Item*>;
-    Weapon* w = new Weapon("",0, 0, "");
-    Armor* a = new Armor("", 0,0,0,0);
-    Trinket* t = new Trinket("",0,0);
+    
     Character* c = new Character();
     StatsManager* sm = new StatsManager();
+    ItemManager* im = new ItemManager();
 
-    User u(1, 2, 3, i, w, a, t, c, sm);
+    User u(1, 2, 3, c, sm, im);
+    
     EXPECT_EQ(u.getEncounterCount(), 3);
 }
 
-TEST(userGetterTests, getUserInventory){
-    std::vector<Item*>* i = new std::vector<Item*>;
-    Weapon* w = new Weapon("",0, 0, "");
-    Armor* a = new Armor("", 0,0,0,0);
-    Trinket* t = new Trinket("",0,0);
-    Character* c = new Character();
-    StatsManager* sm = new StatsManager();
-
-    User u(1, 2, 3, i, w, a, t, c, sm);
-    EXPECT_EQ(u.getInventory(), i);
-}
 
 TEST(userGetterTests, getUserCharacterClass){
-    std::vector<Item*>* i = new std::vector<Item*>;
-    Weapon* w = new Weapon("",0, 0, "");
-    Armor* a = new Armor("", 0,0,0,0);
-    Trinket* t = new Trinket("",0,0);
+    
     Character* c = new Character();
     StatsManager* sm = new StatsManager();
+    ItemManager* im = new ItemManager();
 
-    User u(1, 2, 3, i, w, a, t, c, sm);
+    User u(1, 2, 3, c, sm, im);
+
     EXPECT_EQ(u.getCharacterClass(), c);
 }
 
 TEST(userGetterTests, getUserStatsManager){
-    std::vector<Item*>* i = new std::vector<Item*>;
-    Weapon* w = new Weapon("",0, 0, "");
-    Armor* a = new Armor("", 0,0,0,0);
-    Trinket* t = new Trinket("",0,0);
+    
+
     Character* c = new Character();
     StatsManager* sm = new StatsManager();
+    ItemManager* im = new ItemManager();
 
-    User u(1, 2, 3, i, w, a, t, c, sm);
+    User u(1, 2, 3, c, sm, im);
+
     EXPECT_EQ(u.getStatsManager(), sm);
 }
 
 TEST(userMethodTests, levelUp){
-    User u(1, 5, 10, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr);
+    User u(1, 5, 10, nullptr, nullptr, nullptr);
     u.levelUp();
     EXPECT_EQ(u.getLevel(), 2); 
     u.levelUp();
     EXPECT_EQ(u.getLevel(), 3);
 }
 
-TEST(userMethodTests, equipItem){
-    User u(1, 5, 10, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr);
-    Item* i = new Weapon("sword", 1, 2, "physical");
-    EXPECT_NO_THROW(u.equipItem(i);); // More thought needed
-    delete i;
-}
 
 TEST(userMethodTests, resetEncounterCount){
-    User u(1, 5, 10, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr);
+    User u(1, 5, 10, nullptr, nullptr, nullptr);
     u.incrementEncounterCount();
     EXPECT_EQ(u.getEncounterCount(), 11);
     u.resetEncounterCount();
@@ -116,7 +91,7 @@ TEST(userMethodTests, resetEncounterCount){
 }
 
 TEST(userMethodTests, incrementEncounterCount){
-    User u(1, 5, 10, nullptr, nullptr, nullptr, nullptr, nullptr, nullptr);
+    User u(1, 5, 10, nullptr, nullptr, nullptr);
     EXPECT_EQ(u.getEncounterCount(), 10);
     u.incrementEncounterCount();
     EXPECT_EQ(u.getEncounterCount(), 11);
