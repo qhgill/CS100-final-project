c09d21b8681b4a8ea45cacfa8351e9cb8c01875b
qhgill <qhgill2@gmail.com>
2024-05-29 10:33:43 -0700

    characterSelect screen implementation and testing

--- header/screens/characterSelect.h
+++ header/screens/characterSelect.h
@@ -1,7 +1,14 @@
 #pragma once
 #include <string>
 #include "./screen.h"
+#include "../character.h"
+#include "../warrior.h"
+#include "../mage.h"
+#include "../rogue.h"
+#include "./inGameMenu.h"
 using std::string;
+using std::cout;
+using std::endl;
 
 class CharacterSelect: public Screen{
     public:
--- source/characterSelect.cpp
+++ source/characterSelect.cpp
@@ -1,3 +1,25 @@
 #include "../header/screens/characterSelect.h"
 
 CharacterSelect::CharacterSelect(string dispFile, User* cUser): Screen(dispFile, cUser){}
+
+void CharacterSelect::displayScreen(){
+    Screen::displayFromFile();
+    cout << "Select an option: " << endl;
+    cout << "[1] Warrior" << endl;
+    cout << "[2] Mage" << endl;
+    cout << "[3] Rogue" << endl;
+}
+
+Screen* CharacterSelect::processOption(int option, bool isrunning){
+    if(option == 1){
+        currentUser->getCharacterClass() = new Warrior();
+    } else if(option == 2){
+        currentUser->getCharacterClass() = new Mage();
+    } else if(option == 3){
+        currentUser->getCharacterClass() = new Rogue();
+    } else {
+        return this;
+    }
+    currentUser->getStatsManager()->updateStats(currentUser->getCharacterClass(), currentUser->getItemManager()->getEquippedWeapon(), currentUser->getItemManager()->getEquippedArmor(), currentUser->getItemManager()->getEquippedTrinket(), currentUser->getLevel());
+    return new InGameMenu("gameMenuScreen.txt", currentUser);
+}
