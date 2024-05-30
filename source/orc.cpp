a085138ded1fe75ce1287299edec89d9b673c2de
DavidRHannah <dhann013@ucr.edu>
2024-05-30 05:15:08 -0700

    add unit tests and turn effects

--- source/orc.cpp
+++ source/orc.cpp
@@ -2,11 +2,11 @@
 
 Orc::Orc()
 : Enemy(3, 60, 60, 8, 3, 8, false, "Orc")
-{
-
-}
+{}
 
 void Orc::calculateTurn(StatsManager* sm)
 {
-    
+    // orc's physical intimidation cracks character's physical resistance
+    sm->reducePR(2);
+    sm->takeDamage(this->getDamage()*this->getLevel(), "physical");
 }
--- testing/orcTests.cpp
+++ testing/orcTests.cpp
@@ -1,5 +1,6 @@
 #include <gtest/gtest.h>
 #include "../header/orc.h"
+#include "../header/character.h"
 
 TEST(getterTests, constructorInitializationTest) {
     Orc orc;
@@ -11,6 +12,31 @@ TEST(getterTests, constructorInitializationTest) {
     EXPECT_EQ(orc.getMagicResist(), 3);
     EXPECT_EQ(orc.getPhysicalResist(), 8);
     EXPECT_EQ(orc.getName(), std::string("Orc").c_str());
+}
+
+TEST(damageTests, damageEffectsTest) {
+    Orc orc;
+    Character character;                        // baseHP(10), baseDamage(5), basePhysicalResist(1), baseMagicResist(1)
+    Weapon weapon("fists", 1, 10, "physical");  // itemName, itemLvl, dmg, dmgType
+    Armor armor("cloth", 1, 1, 2, 0);           // itemName, itemLvl, healthBonus, physicalResist, magicalResist
+    Trinket trinket("locket", 1, 2);            // itemName, itemLvl, pierceValue
+    StatsManager statsManager; 
+    
+    statsManager.updateStats(&character, &weapon, &armor, &trinket, 1);
+    
+    // Pre-Turn Stats
+    EXPECT_EQ(statsManager.getCurrentHP(), 21);
+    EXPECT_EQ(statsManager.getCurrentDMG(), 20);
+    EXPECT_EQ(statsManager.getCurrentMR(), 1);
+    EXPECT_EQ(statsManager.getCurrentPR(), 3);
+    
+    orc.calculateTurn(&statsManager);
+    
+    // Post-Turn Stats
+    EXPECT_EQ(statsManager.getCurrentHP(), 0);
+    EXPECT_EQ(statsManager.getCurrentDMG(), 20); 
+    EXPECT_EQ(statsManager.getCurrentMR(), 1); 
+    EXPECT_EQ(statsManager.getCurrentPR(), 3-2); 
 }
 
 int main(int argc, char **argv) {
