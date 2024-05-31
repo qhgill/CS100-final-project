c1045b3ac013518e3fe0d7a386d5966ea3dd06ef
Bestin Watts <bwatt004@xe-01.cs.ucr.edu>
2024-05-30 18:20:27 -0700

    added to bossTest

--- testing/bossTests.cpp
+++ testing/bossTests.cpp
@@ -3,6 +3,13 @@
 #include "../header/bosspart2.h"
 #include "../header/bosspart3.h"
 #include "../header/finalBoss.h"
+#include "../header/statsManager.h"
+#include "../header/weapon.h"
+#include "../header/trinket.h"
+#include "../header/armor.h"
+#include "../header/character.h"
+
+
 
 TEST(getterTests, inheritanceTest) {
     BossPart1 tempBoss;
@@ -15,6 +22,44 @@ TEST(getterTests, inheritanceTest) {
     ASSERT_EQ(tempBoss.getPhysicalResist(), 0);
     ASSERT_EQ(tempBoss.getStatus(), false);
     ASSERT_EQ(tempBoss.getName(), "");
+}
+
+TEST(getterTests, finalBossInheritanceTest) {
+    FinalBoss tempBoss(5, 5, 5, 5, 5, 5, false, "Joe");
+
+    ASSERT_EQ(tempBoss.getLevel(), 5);
+    ASSERT_EQ(tempBoss.getHealth(), 5);
+    ASSERT_EQ(tempBoss.getMaxHealth(), 5);
+    ASSERT_EQ(tempBoss.getDamage(), 5);
+    ASSERT_EQ(tempBoss.getMagicResist(), 5);
+    ASSERT_EQ(tempBoss.getPhysicalResist(), 5);
+    ASSERT_EQ(tempBoss.getStatus(), false);
+    ASSERT_EQ(tempBoss.getName(), "Captain Hook");
+}
+
+TEST(calcTests, finalBossCalculateTurnTest) {
+  FinalBoss tempBoss(10, 10, 10, 10, 10, 10, false, "Captain Hook");
+
+  Character character;
+  Weapon weapon("knife", 5, 5, "physical");
+  Armor armor("helmet", 5, 5, 5, 5);           
+  Trinket trinket("locket", 5, 5);  
+  StatsManager* sm;
+
+  sm->updateStats(&character, &weapon, &armor, &trinket, 1);
+  //  maxHP       =   characterBaseHealth + armorHealthBonus + (userLevel * 10) = 10 + 5 + (1 * 10) = 25
+
+  //  currentMR   =   characterBaseMagicResist + armorMagicResistBonus = 1 + 5 = 6
+  //  currentPR   =   characterBasePhysicalResist + armorPhysicalResistBonus = 1 + 5 = 6
+
+  tempBoss.calculateTurn(sm);
+  ASSERT_EQ(sm->getCurrentHP(), 21);
+
+  tempBoss.dealDamage(8);
+  sm->updateStats(&character, &weapon, &armor, &trinket, 1);
+  tempBoss.calculateTurn(sm);
+  ASSERT_EQ(sm->getCurrentHP(), 13);
+
 }
 
 int main(int argc, char **argv) {
